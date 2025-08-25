13be6c674a69bf7cbe69fafe394ad2deced807ac
Leonardo Arruda Ferreira <147511241+LeoFerreira07@users.noreply.github.com>
2025-08-25 20:59:41 -0300

    feat: Adcionando o Schema do animal

--- src/schemas/animalSchema.js
+++ src/schemas/animalSchema.js
@@ -2,4 +2,12 @@ import { z } from 'zod';
 
 export const animalSchema = z.object({
 
+    nome: z.string(),
+    especie: z.string(),
+    porte: z.string(),
+    vacinado: z.boolean(),
+    adotado: z.boolean(),
+    descricao: z.string(),
+    foto: z.instanceof(Buffer),
+
 })
