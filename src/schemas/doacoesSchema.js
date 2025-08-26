9d85a72a0d077b5b0aa13ffbb9669d88bd3f7f1e
murilobauck <murilobauck2@gmail.com>
2025-08-25 21:48:25 -0300

    chore: nomenclatura doacao para doacoes

--- src/schemas/doacoesSchema.js
+++ src/schemas/doacoesSchema.js
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
-export const doacaoSchema = z.object({
+export const doacoesSchema = z.object({
   nome: z.string(),
   email: z.string().email,
   valor: z.number().positive,
