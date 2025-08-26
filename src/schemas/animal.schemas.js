e482cb77ad1ff44eac1642030cc76e678c3e300a
vitoledo <vitoledors@gmail.com>
2025-08-26 14:40:52 -0300

    chore/alterando-nomenclatura-dos-arquivos

--- src/schemas/animal.schemas.js
+++ src/schemas/animal.schemas.js
@@ -1,13 +1,11 @@
 import { z } from 'zod';
 
 export const animalSchema = z.object({
-
     nome: z.string(),
     especie: z.string(),
     porte: z.string(),
     vacinado: z.boolean(),
     adotado: z.boolean(),
     descricao: z.string(),
-    foto: z.instanceof(Buffer),
-
+    foto: z.instanceof(Buffer)
 })
