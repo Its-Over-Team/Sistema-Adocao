dd338d198f8c55bf69a5c103cb6a2c6ec90756c9
Leonardo Arruda Ferreira <147511241+LeoFerreira07@users.noreply.github.com>
2025-08-25 21:25:36 -0300

    feat: Adcionando Schema tutor

--- src/schemas/tutorSchema.js
+++ src/schemas/tutorSchema.js
@@ -1,5 +1,19 @@
+import e from 'express';
 import { z } from 'zod';
 
 export const tutorSchema = z.object({
-
+    nome_completo: z.string(),
+    email: z.string().email(),
+    senha: z.string(),
+    cidade: z.string(),
+    estado: z.string(),
+    idade: z.number().int(),
+    telefone: z.string(),
+    celular: z.string().optional().nullable(),
+    endereco: z.string().optional().nullable(),
+    bairro: z.string().optional().nullable(),
+    cep: z.number().int().optional().nullable(),
+    instagram: z.string().optional().nullable(),
+    facebook: z.string().optional().nullable(),
+    administrador: z.boolean().optional().nullable(),
 })
