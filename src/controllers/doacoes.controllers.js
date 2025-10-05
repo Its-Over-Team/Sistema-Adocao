103d79fd7cd1ce0ea833522e290c3f7f01f6ad7e
vitoledo <vitoledors@gmail.com>
2025-10-05 11:44:18 -0300

    feat: linkpix e qrcode criados

--- src/controllers/doacoes.controllers.js
+++ src/controllers/doacoes.controllers.js
@@ -1,15 +1,34 @@
+import { gerarPix, gerarQr } from '../lib/pixqrcode'
 import { Doacao } from '../models/Modelos'
 import { doacoesSchema } from '../schemas/doacao.schemas'
-import { ZodError } from 'zod'
+import { email, ZodError } from 'zod'
 
 //POST /doacoes
 export const criarDoacao = async (req, res) => {
   try {
     const data = doacoesSchema.parse(req.body)
 
-    const novaDoacao = await Doacao.create(data)
+    const linkPix = gerarPix(data.valor)
+    const qrCode = gerarQr(linkPix)
 
-    return res.status(201).json(novaDoacao)
+    const doacao = {
+      nome: data.nome,
+      email: data.email,
+      valor: data.valor,
+      mensagem: data.mensagem,
+      linkPix: linkPix,
+    }
+    const doacaoResposta = {
+      nome: data.nome,
+      valor: data.valor,
+      mensagem: data.mensagem,
+      linkPix: linkPix,
+      qrcode: qrCode
+    }
+
+    const novaDoacao = await Doacao.create(doacao)
+
+    return res.status(201).json(doacaoResposta)
   } catch (err) {
     if (err instanceof ZodError) {
       return res.status(400).json({
