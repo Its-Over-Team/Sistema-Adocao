a297f7565f5759391c2947b9120adf363b838ea9
vitoledo <vitoledors@gmail.com>
2025-10-05 14:58:39 -0300

    feat: login feito

---
+++ src/controllers/login.controllers.js
@@ -0,0 +1,32 @@
+import { loginSchema } from '../schemas/login.schemas'
+import { Tutor } from '../models/Modelos'
+import descriptografar from '../lib/encrypt'
+
+export const fazerLogin = async (req, res) => {
+  try {
+    const data = loginSchema.parse(req.body)
+
+    const tutor = await Tutor.findOne({ where: { email: data.email } })
+
+    if (!tutor) {
+      return res.status(401).json({ erro: 'Email ou senha inválidos.' })
+    }
+
+    const senha = descriptografar(tutor.senha)
+
+    if (data.senha !== senha) {
+      return res.status(401).json({ erro: 'Email ou senha inválidos.' })
+    }
+
+    return res.status(200).json({ message: 'Login bem-sucedido' })
+  } catch (err) {
+    if (err instanceof ZodError) {
+      return res.status(400).json({
+        erro: 'Todos os campos obrigatórios devem ser preenchidos corretamente.',
+      })
+    }
+    return res
+      .status(500)
+      .json({ erro: 'Erro interno ao tentar fazer o login.' })
+  }
+}
--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -2,4 +2,4 @@ import { Router } from "express";
 
 const router = Router()
 
-router.post('/', criarLogin)
+router.post('/', fazerLogin)
